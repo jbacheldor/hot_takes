25cf9da9cf33bd74b6de22d9d408ac67ac9f3ed3
bradley <11279144+redSlug@users.noreply.github.com>
2025-02-12 15:49:53 -0500

    add redirect to createhottake page and clean up

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from 'next';
-// import { Geist, Geist_Mono, Orelega_One } from 'next/font/google';
 import './globals.css';
 import localFont from "next/font/local";
 import {Header} from "hottake/components/Header";
-
-// const geistSans = Geist({
-//   variable: '--font-geist-sans',
-//   subsets: ['latin'],
-// });
-
-// const geistMono = Geist_Mono({
-//   variable: '--font-geist-mono',
-//   subsets: ['latin'],
-// });
 
 const orelegaOne = localFont({
   src: "Lemon-Regular.ttf",
@@ -34,7 +23,6 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      {/* <body className={`${geistSans.variable} ${geistMono.variable}`}> */}
       <body className={`${orelegaOne.variable}`}>
       <Header />
       {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 import styles from './page.module.css';
-import HotTakes from 'hottake/app/HotTakes';
+import { redirect } from 'next/navigation'
+
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <HotTakes />
+          {redirect(`/createhottake`)}
       </main>
     </div>
   );

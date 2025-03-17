ba5f64b03a792bdf910ef6e39c1ca64e54183c36
bradley <11279144+redSlug@users.noreply.github.com>
2025-03-17 15:13:47 -0400

    add changes to guessers

--- src/app/guessers/page.tsx
+++ src/app/guessers/page.tsx
@@ -3,7 +3,6 @@ import './results.css';
 import { useEffect, useState } from 'react';
 import {SubHeader} from "hottake/components/SubHeader";
 import {Guessers as GuessersType} from "hottake/types/all";
-import rawGuessers from "../assets/guessers.json";
 
 const initialData =  [
   {
@@ -15,29 +14,28 @@ const initialData =  [
 
 
 function Guessers() {
-  // const pathName = process.env.BASE_URL
+  const pathName = process.env.BASE_URL
   const [guesserData, setGuesserData] = useState<GuessersType>(initialData)
   const [isLoading, setIsLoading]= useState(true)
 
   useEffect(()=> {
-    // const getGuesses = async () => {
-    //   try {
-    //     const res = await fetch(`${pathName}/server/guessers`, {
-    //       method: 'GET'
-    //     });
-    //     const resultData = await res.json();
-    //     console.log('resultData', resultData)
-    //     setGuesserData(resultData)
-    //     setIsLoading(false)
-    //   }catch (e){
-    //     alert('Error!!!!')
-    //     console.log(e)
-    //   }
-    // }
-    // const result = getGuesses()
-    // console.log(result)
+    const getGuesses = async () => {
+      try {
+        const res = await fetch(`${pathName}/server/guessers`, {
+          method: 'GET'
+        });
+        const resultData = await res.json();
+        console.log('resultData', resultData)
+        setGuesserData(resultData)
+        setIsLoading(false)
+      }catch (e){
+        alert('Error!!!!')
+        console.log(e)
+      }
+    }
+    const result = getGuesses()
+    console.log(result)
 
-    setGuesserData(rawGuessers as GuessersType);
     setIsLoading(false)
   }, [])
 

99f552be30bf2d1901c57077fab75b0b4fef4ca5
bradley <11279144+redSlug@users.noreply.github.com>
2025-06-27 16:21:10 -0400

    add github actions for lint, prettier and verifying build

--- src/db/index.ts
+++ src/db/index.ts
@@ -2,7 +2,7 @@ import { drizzle } from 'drizzle-orm/libsql/web';
 
 export const db = drizzle({
   connection: {
-    url: process.env.TURSO_CONNECTION_URL!,
-    authToken: process.env.TURSO_TOKEN!,
+    url: process.env.TURSO_CONNECTION_URL || "libsql://GARBAGE.aws-us-east-1.turso.io",
+    authToken: process.env.TURSO_TOKEN || "GARBAGE",
   },
 });

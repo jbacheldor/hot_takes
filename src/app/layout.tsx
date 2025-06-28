f6c7ed5813f7049cc852dc569cbb925f12adfd67
bradley <11279144+redSlug@users.noreply.github.com>
2025-06-27 20:14:25 -0400

    make sure extra hr does not appear on new game creation page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,13 +21,16 @@ function NavigationBar() {
   }
 
   return (
-    <div id="navigationBar">
-      <a href={'/creategame'}>Create New Game</a>
-      <a href={'/createhottake?game_id=' + game_id}>Create Hot Take</a>
-      <a href={'/castvotes?game_id=' + game_id}>Cast Votes</a>
-      <a href={'/results?game_id=' + game_id}>Results</a>
-      <a href={'/guessers?game_id=' + game_id}>Guessers</a>
-    </div>
+      <>
+        <div id="navigationBar">
+          <a href={'/creategame'}>Create New Game</a>
+          <a href={'/createhottake?game_id=' + game_id}>Create Hot Take</a>
+          <a href={'/castvotes?game_id=' + game_id}>Cast Votes</a>
+          <a href={'/results?game_id=' + game_id}>Results</a>
+          <a href={'/guessers?game_id=' + game_id}>Guessers</a>
+        </div>
+        <hr/>
+      </>
   );
 }
 
@@ -41,9 +44,8 @@ export default function RootLayout({
       <body className={`${orelegaOne.variable}`}>
         <Header />
         <Suspense fallback={<div>loading...</div>}>
-          <NavigationBar />
+          <NavigationBar/>
         </Suspense>
-        <hr />
         <Suspense fallback={<div>loading...</div>}>
           {children}
         </Suspense>
